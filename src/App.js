fd9e1aeda1506fe6504424c171409e22a56c245d
godeh2016 <godeh2016@gmail.com>
2023-07-06 17:05:38 +0200

    css and background color

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Munich" />
         <footer>
           This project was coded by Ghada Odeh and is{" "}
           <a
