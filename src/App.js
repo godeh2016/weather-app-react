25a45abcd9bb07b87e38dfc428ef582a2b04383c
godeh2016 <godeh2016@gmail.com>
2023-07-07 10:38:19 +0200

    Added linkedIn profile and netfliy profile to footer.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,29 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Munich" />
         <footer>
-          This project was coded by Ghada Odeh and is{" "}
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/ghada-odeh/"
+            target="blank"
+            rel="noreferrer"
+          >
+            Ghada Odeh,
+          </a>
+          and is{" "}
           <a
             href="https://github.com/godeh2016/weather-app-react"
             target="_blank"
             rel="noreferrer"
           >
             open-sourced on GitHub
+          </a>
+          and{" "}
+          <a
+            href="https://weather-app-react-ghada-odeh.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
